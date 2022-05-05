a81a1b00c2174d7b95953adbb519cbdd5de09c1b
Erick Tamayo <ericktamayo@gmail.com>
2022-05-05 00:54:57 -0600

    test

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-import {
-  registerMetronome,
-  createMetronomeGetLoadContext,
-} from "@metronome-sh/vercel";
-import * as build from "@remix-run/dev/server-build";
 import { createRequestHandler } from "@remix-run/vercel";
+import * as build from "@remix-run/dev/server-build";
 
-const buildWithMetronome = registerMetronome(build);
-const metronomeGetLoadContext =
-  createMetronomeGetLoadContext(buildWithMetronome);
-
-export default createRequestHandler({
-  build: buildWithMetronome,
-  getLoadContext: metronomeGetLoadContext,
-  mode: process.env.NODE_ENV,
-});
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
