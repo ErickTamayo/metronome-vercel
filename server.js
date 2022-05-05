c63d6e56a6c4206266f5aba3ce71bd4cc295bf19
Erick Tamayo <ericktamayo@gmail.com>
2022-05-05 00:25:50 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import {
+  registerMetronome,
+  createMetronomeGetLoadContext,
+} from "@metronome-sh/vercel";
+import * as build from "@remix-run/dev/server-build";
+import { createRequestHandler } from "@remix-run/vercel";
+
+const buildWithMetronome = registerMetronome(build);
+const metronomeGetLoadContext =
+  createMetronomeGetLoadContext(buildWithMetronome);
+
+export default createRequestHandler({
+  build: buildWithMetronome,
+  getLoadContext: metronomeGetLoadContext,
+  mode: process.env.NODE_ENV,
+});
